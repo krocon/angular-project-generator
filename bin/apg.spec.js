634efb46e1d52014accce7459df8f93e4391308b
Marc Kronberg <krocon@gmail.com>
2020-05-12 17:21:16 +0200

    more tests

--- bin/apg.spec.js
+++ bin/apg.spec.js
@@ -1,38 +1,48 @@
 const path = require('path');
 const exec = require('child_process').exec;
+const fse = require('fs-extra');
 const pkg = require('../package.json');
 
 const testTarget = './test-target';
+const apg = path.resolve('./bin/apg');
+const generatedPAckageJson = path.join(testTarget, 'click-n-ride/package.json');
+
+beforeAll(async () => {
+  await fse.emptyDirSync(testTarget);
+});
 
 test('Exit code should be 0', async () => {
   let result = await cli(['-v'], '.');
   expect(result.code).toBe(0);
-})
+});
 
 test('Version should be ' + pkg.version, async () => {
   let result = await cli(['--version'], '.');
   expect(result.code).toBe(0);
   expect(extractVersion(result.stdout)).toBe(pkg.version);
-})
+});
 
-test('Project should be built', async () => {
-  let result = await cli(['new -a click-n-ride -p app -cp cr -l deutschebahn -f'], testTarget);
+test('Project should be generated', async () => {
+  let result = await cli(['new', '-a', 'click-n-ride', '-p', 'app', '-cp', 'cr', '-l', 'deutschebahn', '-f'], testTarget);
   expect(result.code).toBe(0);
-  console.info(result);
-  expect(extractVersion(result.stdout)).toBe(pkg.version);
-})
+  expect(result.stdout).toMatch('Project successfully generated.');
+
+  let pathExists = fse.pathExistsSync(generatedPAckageJson);
+  expect(pathExists).toBe(true);
+
+}, 6 * 60 * 10000);
 
 
 function extractVersion(stdout) {
   const arr = stdout.split('\n');
-  const v = (arr.splice(arr.length-2).join('')).replace(/ /g, '');
+  const v = (arr.splice(arr.length - 2).join('')).replace(/ /g, '');
   console.info('version: ', v);
   return v;
 }
 
 function cli(args, cwd) {
   return new Promise(resolve => {
-    exec(`node ${path.resolve('./bin/apg')} ${args.join(' ')}`,
+    exec(`node ${apg} ${args.join(' ')}`,
       {cwd},
       (error, stdout, stderr) => {
         resolve({
